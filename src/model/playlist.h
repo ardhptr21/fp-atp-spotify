c259c0a9d630ddb0ffdbe2f72efae1487064340d
DanujaIT24 <udd.muu@gmail.com>
2024-12-02 01:01:38 +0700

    Print Playlist List

--- src/model/playlist.h
+++ src/model/playlist.h
@@ -3,6 +3,7 @@
 #include "../lib/linkedlist.h"
 #include "../lib/util.h"
 
+#define width 32
 namespace playlist
 {
   struct Playlist
@@ -34,4 +35,27 @@ namespace playlist
   {
     linkedlist::removeAt<Playlist>(node, index);
   }
+  void printBorder(char c)
+  {
+    for (int i = 0; i < width * 3 + 1; i++)
+      std::cout << c;
+    std::cout << std::endl;
+  }
+
+  void printPlaylistList(linkedlist::Node<Playlist> *node)
+  {
+    linkedlist::Node<Playlist> *curr = node;
+    printBorder('-');
+    printf("| %-*s | %-*s | %-*s |\n", 10, "ID", width, "PLAYLISTNAME");
+    printBorder('-');
+    int i = 0;
+    while (curr != nullptr)
+    {
+      printf("| %-*d | %-*s | %-*s |\n", 10, i, width, curr->data.id.c_str(), width, curr->data.name.c_str());
+      curr = curr->next;
+      i++;
+    }
+    printBorder('-');
+  }
+
 }
